19257ecf57b2c1e31ae3e009e1047872cee2c989
kyrolyte <dylan.x.h.developer@gmail.com>
2026-03-07 15:58:56 +0000

    fix: adjust scroll delta

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,23 +1,28 @@
 (function () {
-    const nav = document.querySelector('.hextra-nav-container');
-    if (nav) {
-        let lastScrollY = window.pageYOffset;
-        const mobileBreakpoint = 768;
+  const nav = document.querySelector('.hextra-nav-container');
+  if (nav) {
+    let lastScrollY = window.pageYOffset;
+    const mobileBreakpoint = 768;
+    const scrollDelta = 15; 
 
-        window.addEventListener('scroll', () => {
-          if (window.innerWidth > mobileBreakpoint) {
-            nav.classList.remove('nav-hidden');
-            return;
-          }
-          const currentScrollY = window.pageYOffset;
-          if (currentScrollY > lastScrollY && currentScrollY > 50) {
-            nav.classList.add('nav-hidden');
-          } else if (lastScrollY - currentScrollY == 2) {
-            nav.classList.remove('nav-hidden');
-          }
-          lastScrollY = currentScrollY;
-        });
-    }
+    window.addEventListener('scroll', () => {
+      if (window.innerWidth > mobileBreakpoint) {
+        nav.classList.remove('nav-hidden');
+        return;
+      }
+
+      const currentScrollY = window.pageYOffset;
+      const diff = currentScrollY - lastScrollY;
+
+      if (Math.abs(diff) < scrollDelta) return;
+
+      if (diff > 0 && currentScrollY > 50) {
+        nav.classList.add('nav-hidden');
+      } else {
+        nav.classList.remove('nav-hidden');
+      }
+
+      lastScrollY = currentScrollY;
+    });
+  }
 })();
-
-
